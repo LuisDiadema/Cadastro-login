b8d848fa83b67169ad3f7d52c55925b319e2ed08
Luis Gustavo <luis.gustavo.sene99@gmail.com>
2024-06-11 23:41:33 -0300

    UpDate
    Nessa Update, implementei um sistema onde na criação a pessoa tem que escolher um nome de usuário e cria uma senha de 4 dígitos, e ao final ele torna o usuário e senha, porém com a senha escondida

--- src/main/java/io/cadastroLogin/Gustavo/Cadastro.java
+++ src/main/java/io/cadastroLogin/Gustavo/Cadastro.java
@@ -8,19 +8,16 @@ public class Cadastro {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Olá seja Bem-Vindo a nossa pagina de cadastro\n");
-        System.out.println("Digite seu usuario abaixo: \n");
+        System.out.println("Digite seu usuario abaixo:");
 
         String usuario = sc.nextLine();
-        System.out.println("Seu usuario é: " + usuario);
-
-        System.out.println("\nDigite a uma senha abaixo: \n");
-        String password = sc.nextLine();
-        password = password.replaceAll(password, pass);
-        String pass = JOptionPane.showInputDialog(null, "****************");
-        JOptionPane.showConfirmDialog(null, pass);
-
-
-        System.out.println("Seu usuario é: " + usuario + " e sua senha é: " + pass);
-
+        System.out.println("\nDigite uma senha de 4 digitos ");
+        System.out.println("(Somente numeros): ");
+        String pass = sc.nextLine();
+        String aster = pass;
+        for (int i = 0; i < 1; i++) {
+            aster = aster.replaceAll(aster, "****");
+        }
+        System.out.println("\nUsuario: " + usuario + "\nSenha: " + aster);
     }
 }
