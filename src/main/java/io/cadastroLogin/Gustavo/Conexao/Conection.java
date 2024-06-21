2369b5c0a3ac7f703ef04b6d91801a8364e67c4a
Luis Gustavo <luis.gustavo.sene99@gmail.com>
2024-06-21 00:11:11 -0300

    Update Final

--- src/main/java/io/cadastroLogin/Gustavo/Conexao/Conection.java
+++ src/main/java/io/cadastroLogin/Gustavo/Conexao/Conection.java
@@ -5,8 +5,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conection {
-
-
     private static final String url = "jdbc:mysql://localhost:3306/bancodedados";
     private static final String user = "root";
     private static final String pasword = "Gustavo@14";
--- src/main/java/io/cadastroLogin/Gustavo/DAO/userDAO.java
+++ src/main/java/io/cadastroLogin/Gustavo/DAO/userDAO.java
@@ -11,36 +11,37 @@ public class userDAO {
     public void registerUser(user user) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite seu nome completo abaixo: \n");
+        System.out.println("Digite seu nome completo abaixo");
         String name = sc.nextLine();
+        System.out.println("\nNome: " + name);
 
-        System.out.println("\nDigite o seu e-mail: \n");
+        System.out.println("\nDigite o seu e-mail");
         String email = sc.nextLine();
         if (email.contains("@")) {
-            System.out.println("\nSeu e-mail e: " + email);
+            System.out.println("\nE-mail: " + email);
 
         } else if (email.contains("com") || email.contains("com.br")) {
-            System.out.println("\nSeu e-mail é: " + email);
+            System.out.println("\nE-mail: " + email);
 
         } else if (email.contains("gmail") || email.contains("hotmail") || email.contains("yahoo")) {
-            System.out.println("\nSeu e-mail é: " + email);
+            System.out.println("\nE-mail: " + email);
 
         } else {
             System.out.println("\nEstá faltando algo no seu e-mail, tente novamente!!\n");
         }
 
-        System.out.println("\nCrie um usuario: \n");
+        System.out.println("\nCrie um usuario \n");
         String login = sc.nextLine();
+        System.out.println("\nLogin: " + login);
 
-
-        System.out.println("\nCrie uma senha de 4 digitos (Somente números): \n");
+        System.out.println("\nCrie uma senha de 4 digitos (Somente números) \n");
         String senha = sc.nextLine();
         String aster = senha;
         for (int i = 0; i < 1; i++) {
             aster = aster.replaceAll(aster, "****");
         }
 
-        System.out.println("\nCadastro completo confirme as informações a seguir: \n\n" + "Nome: " + name + "\nUsuario: " + login + "\nSenha: " + aster + "\nE-mail: " + email);
+        System.out.println("\nCadastro completo confirme as informações a seguir \n" + "Nome: " + name + "\nUsuario: " + login + "\nSenha: " + aster + "\nE-mail: " + email);
 
         String sql = "INSERT INTO USER (NAME, EMAIL, LOGIN, PASSWORD) VALUES (?, ?, ?, ?)";
 
